01248cab83ba477769c4bed1939c559db739c241
Geronimo Serial <serialgeronimo@gmail.com>
2025-04-30 20:48:29 -0300

    refactor(layout): mejorar la estructura del componente RootLayout y agregar metadatos para SEO
    Este commit actualiza el componente `RootLayout` para incluir etiquetas de metadatos en el encabezado, mejorando la optimización para motores de búsqueda. Además, se ajusta el idioma del HTML a español y se organiza el código para mayor claridad y consistencia.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,44 @@
-import "./index.css"
-import Footer from '../modules/layout/Footer'
-import Header from '../modules/layout/Header'
-import { montserrat } from '../styles/fonts'
-import metadata from './metadata'
+import "./index.css";
+import Footer from "../modules/layout/Footer";
+import Header from "../modules/layout/Header";
+import { montserrat } from "../styles/fonts";
+import metadata from "./metadata";
+import Head from "next/head";
 
-export {metadata}
+export { metadata };
 
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <body className={`${montserrat.className} antialiased`}>
-        <Header/>
-          <div id="root">{children}</div>
-        <Footer/>
-        </body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <body className={`${montserrat.className} antialiased`}>
+        <Header />
+        <div id="root">{children}</div>
+        <Footer />
+      </body>
+    </html>
+  );
+}
