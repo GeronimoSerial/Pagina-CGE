9db1bd1f32dd25abf7a0cd7c3ed22d2e2fdff71c
Geronimo Serial <serialgeronimo@gmail.com>
2025-05-13 12:03:27 -0300

    refactor(skeleton): enhance loading skeleton with shimmer effect and gradient overlay style(swiper): update pagination styles for better alignment and visibility

--- src/components/data/dynamic-client.tsx
+++ src/components/data/dynamic-client.tsx
@@ -5,6 +5,7 @@ import SkeletonLoading from "../SkeletonComponent";
 export const ClientCarousel = dynamic(() => import("../CarouselDeImagenes"), {
   ssr: false,
   loading: () => (
+    // <SkeletonLoading />
     <div className="flex items-center justify-center h-[600px]">
       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#217A4B]"></div>
     </div>
