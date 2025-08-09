73fa2ba35adae0b7685249d132b4a4ff7835476f
Geronimo Serial <serialgeronimo@gmail.com>
2025-08-09 20:13:57 -0300

    refactor: update default image width and correct DIRECTUS_URL for improved image handling

--- src/shared/lib/config.ts
+++ src/shared/lib/config.ts
@@ -1,6 +1,6 @@
 export const SITE_URL =
   process.env.NEXT_PUBLIC_SITE_URL || 'https://consejo.mec.gob.ar';
 export const DIRECTUS_URL =
-  process.env.NEXT_PUBLIC_DIRECTUS_URL || 'https://devcms.geroserial.com';
+  process.env.NEXT_PUBLIC_DIRECTUS_URL || 'https://consejo.geroserial.com';
 
 export const FALLBACK_IMAGE_NEWS = '/public/og-noticias.webp';
